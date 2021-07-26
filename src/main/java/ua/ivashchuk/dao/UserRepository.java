a553f9bcdb128f54f521092c78d6fac11f15b5a6
Java-legioner <ivashchuk.serhii@gmail.com>
2021-07-26 11:36:47 +0300

    Added dao layer using Spring data

---
+++ src/main/java/ua/ivashchuk/dao/UserRepository.java
@@ -0,0 +1,11 @@
+package ua.ivashchuk.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ua.ivashchuk.domain.User;
+
+import java.util.List;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    List<User> findByEmail(String email);
+}
