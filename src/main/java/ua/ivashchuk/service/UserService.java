f76b7a00f1a3bf1875542fdc9b22991c36b0f01c
Java-legioner <ivashchuk.serhii@gmail.com>
2021-07-30 19:01:05 +0300

    Update UserService

---
+++ src/main/java/ua/ivashchuk/service/UserService.java
@@ -0,0 +1,24 @@
+package ua.ivashchuk.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import ua.ivashchuk.dao.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepos;
+
+    public UserService(UserRepository userRepos) {
+        this.userRepos = userRepos;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepos.findByUsername(username);
+    }
+}
